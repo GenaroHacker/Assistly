612c2519eaf4857dfcf9b2f5078fa863eeb8d9bd
GenaroHacker <genarominetto@hotmail.com>
2022-07-24 14:27:49 +0200

    sql_core define la logica, sql_interface ejecuta las funciones

---
+++ sql_interface.py
@@ -0,0 +1,9 @@
+from sql_core import CrearTabla
+from sql_core import InsertarRegistro
+from sql_core import InsertarVariosRegistros
+from sql_core import LeerRegistros
+from sql_core import ActualizarRegistro
+from sql_core import EliminarRegistro
+from sql_core import EjecutarComando
+
+
