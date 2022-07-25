2ecf56653d2fa8e08638d9a0b1618c5f5ecb7133
GenaroHacker <genarominetto@hotmail.com>
2022-07-25 15:19:53 +0200

    conexion de widgets entre .py y .kv, grid simplificada

--- main.py
+++ main.py
@@ -5,10 +5,26 @@ from kivy.uix.gridlayout import GridLayout
 from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
 from kivy.uix.widget import Widget
+from kivy.properties import ObjectProperty
 
 class MyGrid(GridLayout):
+    textinput_month = ObjectProperty(None)
+    label_month = ObjectProperty(None)
+    textinput_week = ObjectProperty(None)
+    label_week = ObjectProperty(None)
+    textinput_day = ObjectProperty(None)
+    label_day = ObjectProperty(None)
+    my_button = ObjectProperty(None)
+
     def btn(self):
-        print("hola")
+        print(self.textinput_month.text)
+        print(self.label_month.text)
+        print(self.textinput_week.text)
+        print(self.label_week.text)
+        print(self.textinput_day.text)
+        print(self.label_day.text)
+        print(self.my_button.text)
+
 
 class architectureApp(App):
     def build(self):
