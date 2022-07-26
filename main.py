482cdb0c9db72605eecf6582120c22cd37a3c6af
GenaroHacker <genarominetto@hotmail.com>
2022-07-26 12:44:37 +0200

    sql_core.py function added and test improved

--- main.py
+++ main.py
@@ -1,15 +1,34 @@
 import kivy
+
 from kivy.app import App
-from kivy.uix.label import Label
+
 from kivy.uix.gridlayout import GridLayout
+from kivy.uix.widget import Widget
+from kivy.uix.label import Label
 from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
-from kivy.uix.widget import Widget
+
 from kivy.properties import ObjectProperty
+
+import time_tools
+
 from sql_interface import CreateTableIfNotExist
+from sql_interface import InsertRecord
+
+
+
+
 
 
 CreateTableIfNotExist()
+
+
+
+
+
+
+
+
 
 
 class MyGrid(GridLayout):
@@ -21,16 +40,24 @@ class MyGrid(GridLayout):
     label_day = ObjectProperty(None)
     my_button = ObjectProperty(None)
 
-    def btn(self):
-        import time_tools
-        from sql_interface import InsertRecord
+    update_month_enabled = False
 
-        if self.textinput_month.text != '':
+
+
+    def btn(self):
+        if self.textinput_month.text != '' and self.update_month_enabled == True:
             txt = "INSERT INTO TableMonth VALUES (NULL,{year},{month},'{theme}')"
             my_sql_command = txt.format(year = time_tools.GetYear(), month = time_tools.GetMonth(), theme = self.textinput_month.text)
-
-
             InsertRecord("Chronos", my_sql_command)
+
+
+
+
+
+
+
+
+
 
 class architectureApp(App):
     def build(self):
